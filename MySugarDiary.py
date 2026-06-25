e5e85130f2970404864fcd505d7102bf6486e636
Prerna-Khandelwal <121911545+Prerna-Khandelwal@users.noreply.github.com>
2026-06-25 21:12:21 +0530

    Update MySugarDiary.py

--- MySugarDiary.py
+++ MySugarDiary.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo  # 👈 ADD THIS LINE
 import requests
 import json
 import time
@@ -63,7 +64,7 @@ st.write("🟢 Database Active | Connected securely across Mobile & PC.")
 st.sidebar.header("➕ Add New Reading")
 with st.sidebar.form(key='log_form', clear_on_submit=True):
     log_date = st.date_input("Date", datetime.now())
-    log_time = st.time_input("Time of Reading", datetime.now().time())
+   log_time = st.time_input("Time of Reading", datetime.now(ZoneInfo("Asia/Kolkata")).time()) 
     timeframe = st.selectbox("Timeframe", ["Morning", "Mid-Morning", "Lunch", "Evening", "Dinner", "Bedtime"])
     
     not_checked = st.checkbox("Not Checked", value=False)
